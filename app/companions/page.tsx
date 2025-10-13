a577a17cb9d7b307aa4c553a012a33045dfb28df
Hunsain Mazhar <Mazhar@DESKTOP-S88HF4T>
2025-10-13 21:37:18 +0500

    implement companion action to fetch companions from db and display them in companions library page

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,33 @@
-const CompanionsLibrary = () => {
+import CompanionCard from "@/components/CompanionCard";
+import { subjectsColors } from "@/constants";
+import { getAllCompanions } from "@/lib/actions/companion.actions"
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : ''
+  const topic = filters.topic ? filters.topic : ''
+
+  const companions = await getAllCompanions({ subject, topic })
+  
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={subjectsColors[companion.subject as keyof typeof subjectsColors]}
+          />
+
+        ))}
+      </section>
+    </main>
   )
 }
 
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -16,3 +16,42 @@ export const createCompanion = async(formData: CreateCompanion) => {
 
     return data[0]    
 }
+
+// Function to get all companions from the database with optional filters and pagination
+export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }: GetAllCompanions) => {
+  // Create a Supabase client instance
+  const supabase = createSupabaseClient();
+
+  // Start building the query by getting 'companions' table
+  let query = supabase
+    .from('companions')
+    .select();
+
+  // Apply filters if subject and topic are provided
+  if (subject && topic) {
+    // Filter by subject and match topic or name
+    query = query
+      .ilike('subject', `%${subject}%`)
+      .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`);
+  }
+  // Apply subject filter only
+  else if (subject) {
+    query = query.ilike('subject', `%${subject}%`);
+  }
+  // Apply topic filter only
+  else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`);
+  }
+
+  // Apply pagination using range (start and end indexes)
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  // Execute the query and get data or error
+  const { data: companions, error } = await query;
+
+  // If there's an error, throw it
+  if (error) throw new Error(error.message);
+
+  // Return the list of companions
+  return companions;
+};
