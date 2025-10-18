9cf556f2ea24e60f6a7fae05d8587927940f803c
Hunsain Mazhar <Mazhar@DESKTOP-S88HF4T>
2025-10-18 14:32:31 +0500

    customize header img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="navbar">
         <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-            <Image src="/images/logo.svg" alt="logo" width={46} height={44}/>
+            <Image src="/images/logo.svg" alt="logo" width={66} height={64}/>
             </div>
         </Link>
         <div className="flex items-center gap-8">
