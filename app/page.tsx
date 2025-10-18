6d74c3078e3f872f4f6679300d8ccd9be148f585
Hunsain Mazhar <Mazhar@DESKTOP-S88HF4T>
2025-10-18 14:43:31 +0500

    make page dynamic like ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,39 @@
-import CompanionCard from "@/components/CompanionCard"
-import CompanionsList from "@/components/CompanionsList"
-import CTA from "@/components/CTA"
-import { recentSessions, subjectsColors } from "@/constants"
-import { getAllCompanions, getRecentSessions } from "@/lib/actions/companion.actions"
+export const dynamic = 'force-dynamic';
+
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions, subjectsColors } from "@/constants";
+import { getAllCompanions, getRecentSessions } from "@/lib/actions/companion.actions";
 
 const Page = async () => {
-  const companions = await getAllCompanions({limit: 3})
-  const recentSessionsCompanions = await getRecentSessions(10)
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
-      <section className="home-section">
 
-        {/* Fetching all companions from db */}
-        {companions.map((companion)=>(
+      <section className="home-section">
+        {companions.map((companion) => (
           <CompanionCard
-          key={companion.id}
-          {...companion}
-          color= {subjectsColors[companion.subject as keyof typeof subjectsColors]}
+            key={companion.id}
+            {...companion}
+            color={subjectsColors[companion.subject as keyof typeof subjectsColors]}
           />
         ))}
-
       </section>
-      
+
       <section className="home-section">
         <CompanionsList
-        title="Recently Completed Sessions"
-        companions={recentSessionsCompanions}
-        classNames="w-2/3 max-lg:w-full"
+          title="Recently Completed Sessions"
+          companions={recentSessionsCompanions}
+          classNames="w-2/3 max-lg:w-full"
         />
-        <CTA/>
+        <CTA />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
